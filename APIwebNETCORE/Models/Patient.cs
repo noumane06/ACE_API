ac44a20beec77790f208bad67c2d06f9c3c2b7fd
Nor_mane <noumane.06@gmail.com>
2022-01-08 23:26:37 +0100

    first commit

---
+++ APIwebNETCORE/Models/Patient.cs
@@ -0,0 +1,16 @@
+﻿namespace APIwebNETCORE.Models
+{
+    public class Patient
+    {
+        public string Num { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Adress { get; set; }
+
+        public Boolean Malade { get; set; }
+        public int Score { get; set; }
+    }
+}
